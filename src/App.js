48a82647b1e7681e4f10f7a6aaa58616403d11c1
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-12-14 13:55:27 +0100

    Create bootstrap with Create-React-App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+/* eslint-enable */
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
