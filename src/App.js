4b6fbf446ddb236a286e45f01650677c2a975824
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-12-14 16:52:12 +0100

    Setting up a stylus and css modules

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 /* eslint-enable */
-import './App.css';
+import styles from './App.styl';
 
 class App extends Component {
   render() {
-    const headerStyles = {
-      height: "150px",
-      width: "100%"
-    };
     return (
-      <div className="App">
-        <header style={headerStyles}>
+      <div className={styles.App}>
+        <header className={styles.Header}>
           <h1>QUESTIONS</h1>
         </header>
         <main className="questionWall">
