15cc8d564886eb19c657d1e0a39ce68b520f3a9e
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-12-14 14:30:19 +0100

    First elements

--- src/App.js
+++ src/App.js
@@ -5,9 +5,38 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const headerStyles = {
+      height: "150px",
+      width: "100%"
+    };
     return (
       <div className="App">
-
+        <header style={headerStyles}>
+          <h1>QUESTIONS</h1>
+        </header>
+        <main className="questionWall">
+          <div className="question">
+            <img src="" alt="" className="logo"/>
+            <div className="heading">
+              <p>Nick is asking</p>
+              <h2>Some question?</h2>
+            </div>
+          </div>
+          <div className="question">
+            <img src="" alt="" className="logo"/>
+            <div className="heading">
+              <p>Nick is asking</p>
+              <h2>Some question?</h2>
+            </div>
+          </div>
+          <div className="question">
+            <img src="" alt="" className="logo"/>
+            <div className="heading">
+              <p>Nick is asking</p>
+              <h2>Some question?</h2>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
