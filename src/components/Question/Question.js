b0c448447f1b2b7005161f9e89953d8ebd36b6af
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-12-16 11:06:46 +0100

    Update to tab styling

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Question.styl';
 import Tab from './Tab/Tab';
 
-const Question = (props) => {
-  return (
-    <div className={styles.Question}>
-      <div className={styles.heading}>
-        <img src="http://via.placeholder.com/50x50" alt="" className={styles.logo}/>
-        <div>
-          <p><span className={styles.username}>Nick</span> is asking</p>
-          <h2 className={styles.title}>Some question?</h2>
+class Question extends Component {
+
+  // state = {
+  //   width: window.innerWidth || document.body.clientWidth
+  // }
+
+  // componentDidMount() {
+  //   window.addEventListener("resize", this.updateDimensions);
+  // }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener("resize", this.updateDimensions);
+  // }
+
+  // updateDimensions = () => {
+  //   this.setState({width: window.innerWidth || document.body.clientWidth})
+  // }
+  render() {
+    let tabs;
+
+    tabs = (
+      <div className={styles.tab}>
+        <Tab type="counter"/>
+        <Tab />
+      </div>
+    )
+
+    // if (this.state.width >= 500) {
+    //   tabs = (
+    //     <div className={styles.tab}>
+    //       <Tab />
+    //       <Tab type="counter"/>
+    //       <Tab />
+    //      </div>
+    //   )
+    // }
+
+    return (
+      <div className={styles.Question}>
+        <div className={styles.heading}>
+          <img src="http://via.placeholder.com/50x50" alt="" className={styles.logo}/>
+          <div>
+            <p><span className={styles.username}>Nick</span> is asking</p>
+            <h2 className={styles.title}>Some question?</h2>
+          </div>
+        </div>
+        <div className={styles.activities}>
+          {tabs}
+          <div className={styles.summary}>
+            <p>1 <i>related discussion</i></p>
+            <p>8 <i>peers invloved</i></p>
+            <p>3 <i>conversations</i></p>
+          </div>
         </div>
       </div>
-      <div className={styles.activities}>
-        <div className={styles.tab}>
-          <Tab type="counter"/>
-          <Tab />
-        </div>
-        <div className={styles.summary}>
-          <p>1 <i>related discussion</i></p>
-          <p>8 <i>peers invloved</i></p>
-          <p>3 <i>conversations</i></p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 export default Question;
--- src/components/Question/Tab/Tab.js
+++ src/components/Question/Tab/Tab.js
@@ -3,10 +3,12 @@ import styles from './Tab.styl';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 const Tab = (props) => {
+  let dot = (<div className={styles.dot}></div>);
   let tab = (
     <div className={styles.Tab}>
       <img src="http://via.placeholder.com/50x50" alt="" className={styles.logo}/>
       <p className={styles.foot}>COMMENTED</p>
+      {dot}
     </div>
   )
 
@@ -15,6 +17,7 @@ const Tab = (props) => {
         <div className={styles.Tab}>
           <p className={styles.counter}>6</p>
           <p className={styles.foot}><i>more activities</i></p>
+          {dot}
         </div>
       );
   }
@@ -22,6 +25,7 @@ const Tab = (props) => {
   return (
     <Aux>
       {tab}
+
     </Aux>
   );
 }
