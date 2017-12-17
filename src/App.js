853f71ed8f5d82c345fffc83a6abdd08a9c3d2d7
Mateusz Niestrój <niestroj.mateusz@gmail.com>
2017-12-17 14:17:03 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 /* eslint-enable */
 import Header from './components/UI/Header/Header';
 import Questions from './containers/Questions/Questions';
+import FullQuestion from './containers/FullQuestion/FullQuestion';
 import styles from './App.styl';
 
 class App extends Component {
   render() {
+    let routes =(
+      <Switch>
+        {/* <Route path="/question" component={} */}
+        <Route path="/" exact component={Questions} />
+        <Route path="/question" component={FullQuestion} />
+      </Switch>
+    )
     return (
       <div className={styles.App}>
         <Header />
-        <Questions />
+        {routes}
       </div>
     );
   }
